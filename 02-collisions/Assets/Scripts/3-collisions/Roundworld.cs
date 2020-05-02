569027876581e6157e1e49abde01c90d6becfb66
Arbel Nathan <arbel.nathan0@gmail.com>
2020-05-02 22:46:37 +0300

    added secne round world

---
+++ 02-collisions/Assets/Scripts/3-collisions/Roundworld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class Roundworld : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == triggeringTag) {
+            transform.position = new Vector3(-transform.position.x, transform.position.y,0);
+        }
+    }
+}
